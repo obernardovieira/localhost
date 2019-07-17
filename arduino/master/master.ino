3c44171bbd8c250b71350eda45a34d00a21eb4e6
Bernardo Vieira <bernardo5@live.com.pt>
2019-07-17 20:10:23 +0100

    update master.ino

--- arduino/master/master.ino
+++ arduino/master/master.ino
@@ -1,18 +1,12 @@
-#include "nrf.h"
 #include "wifi.h"
 
-#define NODE_ID 0
-
-NRF nrf;
 Wifi wifi;
 
 void setup() {
     Serial.begin(115200);
-    nrf.setup(NODE_ID, false);
     wifi.setup();
 }
 
 void loop() {
-    nrf.loop();
     wifi.loop();
 }
